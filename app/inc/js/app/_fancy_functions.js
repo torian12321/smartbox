d4dc8be813c7cca9592531ba1acd6c180cf32d39
Aitor Palomares <aitorpalomares@gmail.com>
2017-01-26 14:37:14 +0000

    smartbox text on the console

--- app/inc/js/app/_fancy_functions.js
+++ app/inc/js/app/_fancy_functions.js
@@ -6,41 +6,26 @@ fancy_functions = (function(_doc){
 
 	var
 	_run = function(){
-		// It gates data from manifest.js on project root
-	    loadJSON('manifest.json', function(response) {
-	        var
-	        appData = JSON.parse(response),
-	        text    = appData.name + ' - ' + appData.description;
-
-	        _sayHello(text, appData.theme_color);
-	        _animateTitle();
-	    });
+		_sayHello('SMARTBOX');
+		//_animateTitle();
 	},
-	_sayHello = function(console_text, color){
+	_sayHello = function(console_text){
 		// http://codepen.io/SamEureka/pen/GZVJvE?editors=0010
 		// Check the console
 		var
 		css_shadow = '',
-		lum        = 0,
-		hex        = String(color).replace(/[^0-9a-f]/gi, ''),
-		rgb        = '',
-		c,
 		i;
 
-		for (i = 0; i < 3; i++) {
-			c = parseInt(hex.substr(i*2,2), 16);
-			c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
-			rgb += ("00"+c).substr(c.length);
-		}
 
-
-		for(i=-1; i<5; i++){
-			css_shadow += i +'px '+ i +'px hsl('+ (200 + (5 * i)) +', 100%, 50%),';
-		}
+		for(i=-1; i<3;  i++){ css_shadow += i +'px '+ i +'px hsl(176, 100%, 33%),'; }
+		for(i=3;  i<6;  i++){ css_shadow += i +'px '+ i +'px hsl(20, 83%, 53%),'; }
+		for(i=6;  i<9;  i++){ css_shadow += i +'px '+ i +'px hsl(342, 100%, 36%),'; }
+		for(i=9;  i<12; i++){ css_shadow += i +'px '+ i +'px hsl(44, 62%, 53%),'; }
+		for(i=12; i<15; i++){ css_shadow += i +'px '+ i +'px hsl(285, 36%, 43%),'; }
 		css_shadow = css_shadow.slice(0, -1); //remove last coma
 
-		var css = 'text-shadow: '+ css_shadow +';font-size: 50px; color: '+ color +';';
-		console.log('%c %s', css, '\ud83c\udf10 '+ console_text);
+		var css = 'text-shadow: '+ css_shadow +';font-size: 50px; color: #FFF;';
+		console.log('%c %s', css, console_text);
 	},
 	_animateTitle = function(){
 		// Add animaitions to the website title (browser tab)
@@ -59,38 +44,6 @@ fancy_functions = (function(_doc){
 			_doc.title = animation[index % animation_length] + title + animation[index % animation_length];
 			index++;
 	    }, 250);
-	},
-	_animateTitle_pet = function(){
-		var
-		index        = 0,
-		title_window = 10,
-		title_void   = Array((title_window + 1)).join('-'),
-		title        = title_void + _doc.title + title_void,
-		title_length = title.length,
-		title_focus  = title_window,
-		pet          = [
-			"┏(^-^)┓",
-			"┗(^-^)┓",
-			"┏(^-^)┓",
-			"┏(^o^)┛",
-			"┗(^-^)┛",
-			"┗(^-^)┛",
-			"┗(^o^)┛",
-			"┏(^o^)┓",
-			"┗(^o^)┛",
-			"┏(^o^)┓"
-		],
-	    pet_length = pet.length;
-
-	    setInterval(function(){
-	    	title_focus = (title_focus != title_length) ? title_focus + 1 : title_window;
-	    	index++;
-
-			_doc.title =
-				pet[index % pet_length] +
-				'  '
-				+ title.substring((title_focus - title_window), title_focus);
-	    }, 400);
 	};
 
 	return {
