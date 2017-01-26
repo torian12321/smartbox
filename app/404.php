a1e587b1e8cb80f285a0d660764eb1f1729a9c49
Aitor Palomares <aitorpalomares@gmail.com>
2017-01-26 14:36:56 +0000

    404 page

--- app/404.php
+++ app/404.php
@@ -1,8 +1,19 @@
-<?php
-/* Redirect browser */
-require_once __dir__ . '/inc/php/init.php';
-sendToHome();
+<!DOCTYPE html>
+<html>
+    <head>
+    	<meta charset="UTF-8">
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
+    	<title>404 page</title>
+    	<style>#errorPage{padding:0 20px;margin:50px auto;display:block;max-width:500px;text-align:center;color:grey;}h1{color:#2a85ce;}.pet{display:block;margin:30px;font-size:32px;}</style>
+    </head>
 
-/* Make sure that code below does not get executed when we redirect. */
-exit;
-?>
+
+    <body>
+        <div id="errorPage">
+            <h1>404</h1>
+            <span class="pet">(ﾉ◕_◕)ﾉ ︵ ┻━┻</span>
+            <span>Oops! This webpage can’t be delivered.</span>
+        </div>
+    </body>
+</html>
